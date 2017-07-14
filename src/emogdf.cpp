615e784b4b610b58973a79d94ce2f47497a633c3
Yosuke ONOUE <onoue@likr-lab.com>
2017-07-14 16:33:12 +0900

    update

--- src/emogdf.cpp
+++ src/emogdf.cpp
@@ -1,23 +1,23 @@
 #include <emscripten/bind.h>
 
-// void defineBasic();
-// void defineEnergybased();
-// void defineFileformats();
-// void defineLayered();
-// void defineMisclayout();
-// void definePacking();
-// void definePlanarity();
-// void defineTree();
-// void defineUpward();
+void defineBasic();
+void defineEnergybased();
+void defineFileformats();
+void defineLayered();
+void defineMisclayout();
+void definePacking();
+void definePlanarity();
+void defineTree();
+void defineUpward();
 
 EMSCRIPTEN_BINDINGS(OGDF) {
-  // defineBasic();
-  // defineEnergybased();
-  // defineFileformats();
-  // defineMisclayout();
-  // defineLayered();
-  // definePacking();
-  // definePlanarity();
-  // defineTree();
-  // defineUpward();
+  defineBasic();
+  defineEnergybased();
+  defineFileformats();
+  defineMisclayout();
+  defineLayered();
+  definePacking();
+  definePlanarity();
+  defineTree();
+  defineUpward();
 }
